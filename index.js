d8198d5f6a191bbf0744b56c59b3400003a3df13
Notebooked <creepinglion09@gmail.com>
2024-06-15 11:04:06 -0700

    test custom parsing

--- index.js
+++ index.js
@@ -38,6 +38,28 @@ class JSOParser extends Parser {
             return(super.parseStatement(context, topLevel, exports));
         }
     }
+
+    parseMyStatement(node) {
+        console.log("parse function called");
+        this.next();
+  
+        //In my language, MyStatement doesn't have to have a parameter. It could be called as `MyStatement { ... }`
+        if (this.type == tt.parenL) {
+            node.test = this.parseOptionalParenExpression();
+        }
+        else {
+            node.test = 0; //If there is no test, just make it 0 for now (note that this may break code generation later).
+        }
+  
+        node.isMyStatement = true; //set a flag so we know that this if a "MyStatement" instead of an if statement.
+  
+        //process the body of the block just like a normal if statement for now.
+  
+        // allow function declarations in branches, but only in non-strict mode
+        node.consequent = this.parseStatement("if");
+        //node.alternate = this.eat(acornTypes["else"]) ? this.parseStatement("if") : null;
+        return this.finishNode(node, "IfStatement");
+    }
 }
 
 const inCode = fs.readFileSync("./test.jso");
